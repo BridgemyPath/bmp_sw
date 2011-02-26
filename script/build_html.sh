6ca815c47b8cdfd79d4e270d9b5e1a2199d950d5
Peter Lyons <pete@peterlyons.com>
2011-02-26 16:35:54 -0700

    build script updated for register page

--- script/build_html.sh
+++ script/build_html.sh
@@ -3,5 +3,5 @@ BASE='http://localhost:9600'
 curl --silent --output "public/index.html" "${BASE}/"
 for PAGE in signup register
 do
-    curl --silent --output "public/${PAGE}.html" "${BASE}/signup"
+    curl --silent --output "public/${PAGE}.html" "${BASE}/${PAGE}"
 done
